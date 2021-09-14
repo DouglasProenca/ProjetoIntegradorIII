faeb01acd1430cb1b6c243632920a3dccfb87fc3
Camiilo-Rafa <72233498+Camiilo-Rafa@users.noreply.github.com>
2021-09-14 20:24:17 -0300

    Alterando consulta de alunos e colaboradores

--- prototipo_telas_pi3/src/main/java/view/cadastroDeAlunos.java
+++ prototipo_telas_pi3/src/main/java/view/cadastroDeAlunos.java
@@ -195,10 +195,13 @@ public class cadastroDeAlunos extends javax.swing.JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
+        jButton1.setBackground(new java.awt.Color(51, 255, 51));
         jButton1.setText("SALVAR");
 
+        jButton2.setBackground(new java.awt.Color(255, 51, 51));
         jButton2.setText("CANCELAR");
 
+        jButton4.setBackground(new java.awt.Color(51, 51, 255));
         jButton4.setText("CONSULTAR");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -214,11 +217,11 @@ public class cadastroDeAlunos extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(22, 22, 22)
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(48, 48, 48)
+                .addGap(55, 55, 55)
                 .addComponent(jButton4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(47, 47, 47))
+                .addGap(29, 29, 29))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
