a4bebb65755096fbe07a4e37808d3b9807311fc9
Douglas Proenca <78769051+DouglasProenca@users.noreply.github.com>
2021-10-31 16:34:18 -0300

    Melhorando Login

--- ProjetoIntegradorIII/src/main/java/br/senac/conexaobd/servlet/LoginServlet.java
+++ ProjetoIntegradorIII/src/main/java/br/senac/conexaobd/servlet/LoginServlet.java
@@ -17,17 +17,17 @@ import javax.servlet.http.HttpSession;
  * @author Douglas
  */
 public class LoginServlet extends HttpServlet {
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
             String nomeUsuario = request.getParameter("nomeUsuario");
             String senhaUsuario = request.getParameter("senhaUsuario");
-            
+
             Usuario usuario = UsuarioDAO.getUsuario(nomeUsuario, senhaUsuario);
             if (usuario == null) {
-                
+
             } else {
                 HttpSession sessao = request.getSession();
                 sessao.setAttribute("usuario", usuario);
@@ -40,5 +40,16 @@ public class LoginServlet extends HttpServlet {
             Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println(ex);
         }
+    }
+
+    //Logout
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession sessao = request.getSession();
+        sessao.invalidate();
+        response.sendRedirect(request.getContextPath() + "Login.jsp");
+
     }
 }
--- ProjetoIntegradorIII/src/main/webapp/protegido/index.jsp
+++ ProjetoIntegradorIII/src/main/webapp/protegido/index.jsp
@@ -11,7 +11,7 @@
   </head>
 
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="${pageContext.request.contextPath}/protegido/index.jsp">
+      <a class="navbar-brand" href="${pageContext.request.contextPath}/Login.jsp">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16">
           <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"></path>
         </svg>
@@ -91,7 +91,7 @@
 
   <div class="row featurette">
     <div class="col-md-7">
-      <h2 class="featurette-heading">Bem-vindo ao Sistema</h2>  
+        <h2 class="featurette-heading">Bem-vindo, ${sessionScope.usuario.pessoa} ao Sistema</h2>  
     </div>
   </div>
 
